791675c247e780ee7e01dc15bb23700d1bda64b7
haniejnsh <h.janeshinpour1995@gmail.com>
2024-11-04 12:26:38 +0330

    add header ui

--- code-challenge/src/components/Header.tsx
+++ code-challenge/src/components/Header.tsx
@@ -1,5 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className="flex gap-2 py-5 px-6 bg-gray-200 text-gray-800 ">
+        <div className="w-[40%] text-xl text-gray-600 font-bold">veempars code challenge</div>
+        <div className="flex justify-start gap-44 text-lg font-bold w-[60%]">
+            <NavLink to={"/"} className={({isActive})=> isActive?"text-red-400":"text-gray-500 hover:text-red-300 transition"}>home</NavLink>
+            <NavLink to={"/add"} className={({isActive})=> isActive?"text-red-400":"text-gray-500 hover:text-red-300 transition"}>add</NavLink>
+            <NavLink to={"/login"} className={"text-gray-500 hover:text-red-300 transition"}>login</NavLink>
+        </div>
+    </div>
   )
 }
