d5ffaf3129d9c2f7a48e470e8371c61316bf872c
haniejnsh <h.janeshinpour1995@gmail.com>
2024-11-04 12:52:15 +0330

    header responsive and change ui color

--- code-challenge/src/components/Header.tsx
+++ code-challenge/src/components/Header.tsx
@@ -2,12 +2,12 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="flex gap-2 py-5 px-6 bg-gray-200 text-gray-800 ">
-        <div className="w-[40%] text-xl text-gray-600 font-bold">veempars code challenge</div>
-        <div className="flex justify-start gap-44 text-lg font-bold w-[60%]">
-            <NavLink to={"/"} className={({isActive})=> isActive?"text-red-400":"text-gray-500 hover:text-red-300 transition"}>home</NavLink>
-            <NavLink to={"/add"} className={({isActive})=> isActive?"text-red-400":"text-gray-500 hover:text-red-300 transition"}>add</NavLink>
-            <NavLink to={"/login"} className={"text-gray-500 hover:text-red-300 transition"}>login</NavLink>
+    <div className="flex flex-col md:flex-row items-center gap-6 py-5 px-6 bg-gray-100 shadow-md shadow-gray-200">
+        <div className="md:w-[40%] text-xl text-emerald-800 font-bold">veempars code challenge</div>
+        <div className="flex justify-around w-full lg:justify-start lg:gap-44 text-lg font-bold md:w-[60%]">
+            <NavLink to={"/"} className={({isActive})=> isActive?"text-emerald-400":"text-gray-500 hover:text-emerald-300 transition"}>home</NavLink>
+            <NavLink to={"/add"} className={({isActive})=> isActive?"text-emerald-400":"text-gray-500 hover:text-emerald-300 transition"}>add</NavLink>
+            <NavLink to={"/login"} className={"text-gray-500 hover:text-emerald-400 transition"}>login</NavLink>
         </div>
     </div>
   )
