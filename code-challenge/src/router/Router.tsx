34446b203ff4aec5930e62e8520c0cb14fd84a03
haniejnsh <h.janeshinpour1995@gmail.com>
2024-11-04 11:24:43 +0330

    create pages components

---
+++ code-challenge/src/router/Router.tsx
@@ -0,0 +1,14 @@
+import MainLayout from "../layouts/MainLayout";
+
+const routes=([
+    {
+        path:"/",
+        element:<MainLayout/>,
+        Children:[
+            {
+                index:true,
+                element:
+            }
+        ]
+    }
+])
