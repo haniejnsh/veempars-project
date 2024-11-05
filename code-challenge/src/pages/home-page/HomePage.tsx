b588fccbef98d368b0132a0142e28ceaaa70918f
haniejnsh <h.janeshinpour1995@gmail.com>
2024-11-05 14:09:32 +0330

    complete pagination

--- code-challenge/src/pages/home-page/HomePage.tsx
+++ code-challenge/src/pages/home-page/HomePage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import useGet from "../../hooks/useGet";
 import { TODO_URL } from "../../services/api";
 import Pagination from "./components/Pagination";
@@ -11,9 +12,16 @@ interface taskType {
 }
 
 export default function HomePage() {
+  const [page,setPage]=useState(0)
   
  
-  const {data,isLoading,isError}=useGet(`${TODO_URL}?limit=10&skip=10`)
+  const {data,isLoading,isError,refetch}=useGet(`${TODO_URL}?limit=10&skip=${page}`)
+
+  useEffect(
+    ()=>{refetch()}
+  ,[page,refetch])
+
+
   if(isLoading){
     return(
       <div className="text-emerald-600 flex w-full items-center justify-center text-xl pt-20 font-bold" >is loading . . .</div>
@@ -24,8 +32,9 @@ export default function HomePage() {
       <div className="text-red-600 flex w-full items-center justify-center text-xl pt-20 font-bold">Error occurred</div>
     )
   }
-  console.log(data,data?.todos);
-  
+  // console.log(data);
+  const dataTotal=data?.total as number
+  const totalPage:number=Math.ceil(dataTotal/10) 
   return (
     <div className="flex flex-col w-full items-center  pt-10 pb-6 px-8 gap-8">
       <h1 className="text-2xl font-bold text-emerald-800">
@@ -41,7 +50,7 @@ export default function HomePage() {
         }
       </div>
       <div className="py-4">
-        <Pagination/>
+        <Pagination  totalPage={totalPage} pageNumber={page} pageCounter={p => setPage(p)}/>
       </div>
 
     </div>
--- code-challenge/src/pages/home-page/components/Pagination.tsx
+++ code-challenge/src/pages/home-page/components/Pagination.tsx
@@ -1,15 +1,32 @@
 import { CgPlayTrackNext, CgPlayTrackPrev ,  } from "react-icons/cg";
 
-export default function Pagination() {
+interface propsType {
+    totalPage:number;
+    pageCounter:(p: number) => void;
+    pageNumber:number;
+}
+
+export default function Pagination({totalPage , pageCounter,pageNumber}:propsType) {
+    const next=()=>{
+        if(((pageNumber/10)+1)<totalPage){
+            pageCounter(pageNumber+10)
+        } 
+    }
+    const prev=()=>{
+        if(pageNumber>=10){
+            pageCounter(pageNumber-10)
+        } 
+    }
+    
   return (
     <div className="flex items-center gap-6 text-emerald-800">
-        <CgPlayTrackPrev className="text-2xl text-emerald-800 hover:text-emerald-500 cursor-pointer transition mr-2"/>
+        <CgPlayTrackPrev onClick={prev} className="text-2xl text-emerald-800 hover:text-emerald-500 cursor-pointer transition mr-2"/>
         <div className="flex items-center gap-1">
-            <span className="bg-emerald-500 w-5 h-5 rounded-full text-white flex justify-center items-center font-bold">1</span>
+            <span className="bg-emerald-500 w-5 h-5 rounded-full text-white flex justify-center items-center font-bold">{(pageNumber/10)+1}</span>
             <span className="text-xs">/</span>
-            <span className="text-xs pt-1">12</span>
+            <span className="text-xs pt-1">{totalPage}</span>
         </div>
-        <CgPlayTrackNext className="text-2xl text-emerald-800 hover:text-emerald-500 cursor-pointer transition"/>
+        <CgPlayTrackNext onClick={next} className="text-2xl text-emerald-800 hover:text-emerald-500 cursor-pointer transition"/>
     </div>
   )
 }
